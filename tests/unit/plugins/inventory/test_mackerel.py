ded43194a5b83f62e53d723ddaf41c450caa3140
xcezx <main.xcezx@gmail.com>
2020-03-16 01:03:08 +0900

    fix test_verify_file

--- tests/unit/plugins/inventory/test_mackerel.py
+++ tests/unit/plugins/inventory/test_mackerel.py
@@ -12,5 +12,12 @@ def inventory():
     return InventoryModule()
 
 
-def test_verify_file_bad_config(inventory):
-    assert inventory.verify_file('not_mackerel_config.yml') is False
+@pytest.mark.parametrize('path,got', [
+    ('mackerel.yml', True),
+    ('mackerel.yaml', True),
+    ('not_mackerel_config.yml', False),
+])
+def test_verify_file_bad_config(inventory, tmpdir, path, got):
+    config_file = tmpdir.join(path)
+    config_file.ensure()
+    assert inventory.verify_file(str(config_file)) is got
